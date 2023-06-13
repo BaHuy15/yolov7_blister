1a7dccdc3b8c804681d87cda628b2192eb37e7e1
BaHuy15 <huyn0995789648@gmail.com>
2023-06-13 11:34:28 +0800

    modify tracedmodel to train gray_scale image

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -342,13 +342,15 @@ def revert_sync_batchnorm(module):
 
 class TracedModel(nn.Module):
 
-    def __init__(self, model=None, device=None, img_size=(640,640)): 
+    def __init__(self, model=None, device=None, img_size=(640,640),gray_scale=False): 
         super(TracedModel, self).__init__()
         
         print(" Convert model to Traced-model... ") 
         self.stride = model.stride
         self.names = model.names
         self.model = model
+        self.gray_scale=gray_scale
+
 
         self.model = revert_sync_batchnorm(self.model)
         self.model.to('cpu')
@@ -357,7 +359,10 @@ class TracedModel(nn.Module):
         self.detect_layer = self.model.model[-1]
         self.model.traced = True
         
-        rand_example = torch.rand(1, 3, img_size, img_size)
+        if self.gray_scale:
+            rand_example = torch.rand(1, 1, img_size, img_size)
+        else:
+            rand_example = torch.rand(1, 3, img_size, img_size)
         
         traced_script_module = torch.jit.trace(self.model, rand_example, strict=False)
         #traced_script_module = torch.jit.script(self.model)
